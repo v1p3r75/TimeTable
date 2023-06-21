ccef6ee478d9494595bd7c313cfd434042038eb6
v1p3r75 <elfriedv16@gmail.com>
2023-06-21 16:55:47 +0100

    User : Update password

--- TimeTable/views.py
+++ TimeTable/views.py
@@ -19,6 +19,68 @@ def adminDash(request):
 @login_required( login_url = 'login')
 def userAccount(request):
 
+    if request.method == 'POST':
+
+        if request.POST.get('action') == 'edit':
+
+            if request.POST.get('action-detail') == 'info':
+
+                data = {
+                    'id': request.POST.get('id'),
+                    'lastname': request.POST.get('lastname'),
+                    'firstname': request.POST.get('firstname'),
+                    'phone': request.POST.get('phone'),
+                    'email': request.POST.get('email'),
+                    'password': request.POST.get('password'),
+                }
+                
+                if User.objects.get(id = data.get('id')):
+                                    
+                    User.objects.filter(id = data.get('id')).update(**data)
+
+                    return JsonResponse({'success': True, 'message': 'Mise à jour avec succès', 'data': data})
+                
+                return JsonResponse({'success': False, 'message': 'L\'élément est introuvable.'})
+
+            if request.POST.get('action-detail') == 'password':
+
+                data = {
+                    'id': request.POST.get('id'),
+                    'new_password': request.POST.get('new_password'),
+                    'password': request.POST.get('password'),
+                    'password_confirmation': request.POST.get('password_confirmation'),
+                }
+                
+                if (data.get('new_password') != data.get('password_confirmation')):
+                    
+                    return JsonResponse({'success': False, 'message': 'Confirmation de mot passe échoué.'})
+
+                user = User.objects.get(id = data.get('id'))
+
+                if user:
+                        if user.check_password(data.get('password')):
+
+                            user.set_password(data.get('new_password'))
+                            user.save()
+                            return JsonResponse({'success': True, 'message': 'Mise à jour avec succès', 'data': data})
+
+                        return JsonResponse({'success': False, 'message': 'Mot de passe incorrect.'})
+                
+                return JsonResponse({'success': False, 'message': 'L\'élément est introuvable.'})
+
+
+
+        if request.POST.get('action') == 'del':
+
+            if User.objects.get(id = request.POST.get('id')):
+                                
+                User.objects.filter(id = request.POST.get('id')).delete()
+
+                return JsonResponse({'success': True, 'message': 'Supprimer avec succès'})
+            
+            return JsonResponse({'success': False, 'message': 'L\'élément est introuvable.'})
+        
+
     return render(request, 'timetable/account.html')
 
 
