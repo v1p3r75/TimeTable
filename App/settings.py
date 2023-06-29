4a4eea3bd3236c8bc7605f011d9a974fadb6ee7b
v1p3r75 <elfriedv16@gmail.com>
2023-06-30 00:45:42 +0100

    Fixed media images error

--- App/settings.py
+++ App/settings.py
@@ -126,7 +126,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [ BASE_DIR / 'static/' ]
 
-MEDIA = BASE_DIR / 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
+
+MEDIA_URL = '/media/'
 
 AUTH_USER_MODEL = 'Auth.User'
 
